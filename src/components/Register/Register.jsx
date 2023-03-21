777a58bc063623315dab420bb6a750aa4c471ad7
rahimtech <rahimianalytic@gmail.com>
2023-03-21 11:02:25 +0330

    Fixed Signin

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,42 +1,95 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import UserContext from "../context/userContext";
 import CloseIcon from "@mui/icons-material/Close";
+import Axios from "axios";
+import Alert from "@mui/material/Alert";
+import { Button, Input, TextField } from "@mui/material";
 
 const Register = () => {
   const con = useContext(UserContext);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+  const [serverMessage, setServerMessage] = useState("");
+  const [open, setOpen] = useState("");
 
+  const signInFunc = async () => {
+    await Axios.post("http://localhost:3001/signin", {
+      name: name,
+      email: email,
+      pass: pass,
+    })
+      .then((response) => {
+        if (response.status != 200) {
+          setOpen("400");
+          setServerMessage("کاربری با این ایمیل وجود دارد !!");
+        } else {
+          setOpen("200");
+          setServerMessage("با موفقیت ثبت نام شدید :)");
+        }
+      })
+      .catch((err) => {
+        console.log("err: ", err);
+      });
+  };
   return (
     <div
       className={`${con.checkReg} bg-slate-800 w-screen h-screen z-10 items-center`}
     >
       <div
         className="bg-red-500 w-screen fixed h-screen z-20"
-        onClick={() => con.setCheckReg("hidden")}
+        // onClick={() => con.setCheckReg("hidden")}
       ></div>
       <CloseIcon
         className="rounded-full bg-slate-100 text-gray-500 z-30 absolute items-center left-0 right-0 mx-auto top-24 hover:shadow-md hover:cursor-pointer text-center"
         onClick={() => con.setCheckReg("hidden")}
       />
+      <Alert
+        severity="warning"
+        className={`z-30 ${
+          open == 400 ? "absolute" : "hidden"
+        }  rtl items-center left-0 right-0 mx-auto top-36 w-72`}
+      >{`${serverMessage}`}</Alert>
+      <Alert
+        severity="success"
+        className={`z-30 ${
+          open == 200 ? "absolute" : "hidden"
+        } rtl items-center left-0 right-0 mx-auto top-36 w-72`}
+      >{`${serverMessage}`}</Alert>
       <div className="h-screen w-screen flex items-center">
         <div
           className={`flex justify-center w-52 h-auto rtl flex-col mx-auto my-auto p-5 shadow-md bg-white !z-30 rounded-md`}
         >
-          <input
+          <TextField
+            id="standard-basic"
+            label="نام و نام‌خانوادگی"
+            variant="standard"
             type="text"
             placeholder="نام و نام خانوادگی خود را وارد کنید"
-            className="mb-3"
+            className="mb-3 rtl"
+            onBlur={(e) => setName(e.target.value)}
           />
-          <input
+          <TextField
+            id="standard-basic"
+            label="ایمیل"
+            variant="standard"
             type="email"
             placeholder="ایمیل خود را وارد کنید"
-            className="mb-3"
+            className="my-3 rtl"
+            onBlur={(e) => setEmail(e.target.value)}
           />
-          <input
+          <TextField
+            id="standard-basic"
+            label="رمز"
+            variant="standard"
             type="password"
             placeholder="رمز خود را وارد کنید"
-            className="mb-3"
+            className="mb-3 rtl"
+            onBlur={(e) => setPass(e.target.value)}
           />
-          <button type="submit">ثبت نام</button>
+          <Button type="submit" className="mt-5" onClick={() => signInFunc()}>
+            ثبت نام
+          </Button>
         </div>
       </div>
     </div>
--- src/server/index.js
+++ src/server/index.js
@@ -4,24 +4,66 @@ const mysql = require("mysql");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 
-const users = [
-  { id: 1, name: "ali", email: "ddd@gmail.com", pass: 1212 },
-  { id: 12, name: "hassan", email: "fff@gmail.com", pass: 1313 },
-];
-
+//Pre-Confings
 const app = express();
 const PORT = 3001;
 app.use(cors());
 app.use(bodyParser.json());
 
-// //Sign in ...
-// app.get("/signin", (req, res) => {
-//   email = req.body.email;
-//   pass = req.body.pass;
-// });
+//Local Database
+const users = [
+  { id: 1, name: "ali", email: "ddd@gmail.com", pass: 1212 },
+  { id: 12, name: "hassan", email: "fff@gmail.com", pass: 1313 },
+];
 
-//Check Login ...
-app.post("/", (req, res) => {
+//Connect-To-Database
+var db = mysql.createConnection({
+  multipleStatements: true,
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "torob",
+});
+
+//Sign in ...
+app.post("/signin", (req, res) => {
+  db.query(
+    "SELECT * FROM users WHERE email=?",
+    [req.body.email],
+    (err, result) => {
+      console.log("result: ", result.length);
+
+      if (result.length > 0) {
+        res.status(204).send();
+        return;
+      } else {
+        const token = jwt.sign(
+          {
+            username: req.body.email,
+          },
+          "mysupersecretkey",
+          { expiresIn: "6 hours" }
+        );
+        db.query(
+          "INSERT INTO users(id,name,email,pass,token) VALUES(?,?,?,?,?)",
+          [, req.body.name, req.body.email, req.body.pass, token],
+          (err, result) => {
+            if (err) {
+              console.log("err: ", err);
+            } else {
+              res.status(200).send({ access_token: token });
+
+              return;
+            }
+          }
+        );
+      }
+    }
+  );
+});
+
+//Sign Up ...
+app.post("/signup", (req, res) => {
   if (!req.body.email || !req.body.pass) {
     res.send("You Have Problem to Register");
     return;
@@ -45,9 +87,10 @@ app.post("/", (req, res) => {
 
 //Check Another pages ...
 app.get("*", (req, res) => {
-  res.send("NOT FOUND !!!");
+  res.send("Page NOT FOUND !!!");
 });
 
+//Server Running ...
 app.listen(PORT, (err) => {
   if (err) throw err;
   if (!err) {
